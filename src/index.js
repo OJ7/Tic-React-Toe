452f655a5c26531791f5041dc96940fe35a67bda
Omair Javed <omairj7@gmail.com>
2021-04-22 23:04:13 -0400

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -15,13 +15,21 @@ class Board extends React.Component {
   	super(props);
   	this.state = {
   		squares: Array(9).fill(null),
+  		xIsNext: true,
   	};
+  }
+
+  get nextTurn() {
+  	return this.state.xIsNext ? 'X' : 'O';
   }
 
   handleClick(i) {
   	const squares = this.state.squares.slice();
-  	squares[i] = 'X';
-  	this.setState({squares})
+  	squares[i] = this.nextTurn;
+  	this.setState({
+  	  squares,
+  	  xIsNext: !this.state.xIsNext,
+  	})
   }
   
   renderSquare(i) {
@@ -32,7 +40,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + this.nextTurn;
 
     return (
       <div>
